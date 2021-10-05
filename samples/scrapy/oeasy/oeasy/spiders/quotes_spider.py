b06ed825bb4e5287f442c6dddef5f8c725466a02
overmind1980 <easyo@easydeMBP.lan>
2021-10-05 19:16:57 +0800

    yes

---
+++ samples/scrapy/oeasy/oeasy/spiders/quotes_spider.py
@@ -0,0 +1,13 @@
+import scrapy
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
