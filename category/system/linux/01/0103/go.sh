87150ebc9474c10c13e155d33cee4eab920e68ce
overmind1980 <overmind1980@gmail.com>
2024-04-14 22:33:37 +0800

    yes

--- category/system/linux/01/0103/go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for file in $(find . -type d -depth 1);do
-    unit=${file:2};
-    echo $unit;
-    rm -f ./$unit/$unit.html
-    cat head.template ./$unit/$unit.md foot.template>./$unit/$unit.html;
-done
